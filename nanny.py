e3b800b9940b6dd53bce8a3f692a23948cc6b8d8
sakshi-fermi <snag@fnal.gov>
2021-01-22 17:06:21 -0600

    feat: :sparkles: Create nanny script
    nanny handles filename conventions and timestamp calculations.

--- nanny.py
+++ nanny.py
@@ -11,18 +11,15 @@ from glob import glob
 from os import path
 import requests
 import dpmData
+import re
+import isodate
+from datetime import datetime
+from datetime import timedelta
+import sys
 
 DRF_REQUESTS_LIST = 'linac_logger_drf_requests.txt'
 OUTPUTS_DIRECTORY = path.abspath('.')
-
-
-def read_input(file):
-    output = []
-
-    with open(file) as f:
-        output = f.read().splitlines()
-
-    return output
+DURATION = timedelta(hours=1)
 
 
 def write_output(file, output):
@@ -31,18 +28,28 @@ def write_output(file, output):
             f.write(line + '\n')
 
 
+def get_latest_device_list_version():
+    url = 'https://api.github.com/repos/fermi-controls/linac-logger-device-cleaner/releases/latest'
+    response = requests.get(url)
+
+    if response.status_code == requests.codes.ok:
+        return response.json()["name"]
+    else:
+        print('Could not fetch ' + url)
+
+    return None
+
+
 def get_latest_device_list():
     url = 'https://github.com/fermi-controls/linac-logger-device-cleaner/releases/latest/download/linac_logger_drf_requests.txt'
-    req = requests.get(url)
+    response = requests.get(url)
 
-    if req.status_code == requests.codes.ok:
+    if response.status_code == requests.codes.ok:
         device_list = [line.strip()  # Trim whitespace
-                       for line in req.text.split('\n')
+                       for line in response.text.split('\n')
                        if line]  # Remove blank lines
 
         write_output(DRF_REQUESTS_LIST, device_list)
-
-        return device_list
     else:
         print('Could not fetch ' + url)
 
@@ -52,54 +59,68 @@ def get_start_time():
     # TODO: If not, use duration to calculate start time
     # TODO: If so, calculate the most recent data timestamp from OUTPUTS_DIRECTORY filenames
     # output filename convention => `20210101T130000PT10M-1_0_0.h5`
-    # TODO: Get all filenames from OUTPUTS_DIRECTORY
-    # TODO: Sort filenames to get most recent start time
-    # TODO: Calculate start time of most recent file
-    # TODO: Calculate duration of most recent file
-    # TODO: Calculate end time of from start time and duration
-    # TODO: return end time
-
-    # Find all h5 files in OUTPUTS_DIRECTORY
-    # TODO: If a file not following convention is found
-    # TODO: it could always be sorted first. The files should be filtered
-    # TODO: to ensure we are looking at data.
     h5_outputs = path.join(OUTPUTS_DIRECTORY, '*.h5')
-
     # Glob allows the use of the * wildcard
     files = glob(h5_outputs)
     # Sort modifies the list in place
     files.sort()
-    most_recent_filename = path.basename(files[-1])
-    date_time = most_recent_filename.split('P')[0]
-    date, time = date_time.split('T')
-    print(date, time)
+    # Flag which keeps the while loop running until a valid filename is found
 
-    return 'start_time'
+    while len(files) > 0:
+        try:
+            most_recent_filename = path.basename(files[-1])
+            date_time_duration_str = most_recent_filename.split('-')[0]
+            date_time_str, duration_str = date_time_duration_str.split('P')
+            start_time = isodate.parse_datetime(date_time_str)
+            duration = isodate.parse_duration('P' + duration_str)
+            end_time = start_time + duration
+            return end_time, duration
+        except:
+            files.pop()
+
+    # TODO: determine start_time and duration without existing filename
+    end_time = datetime.now()
+    start_time = end_time - DURATION
+    return start_time, DURATION
 
 
-def main():
+def name_output_file(start_time, duration=None):
+    start_time_str = isodate.datetime_isoformat(start_time)
+    if duration is not None:
+        duration_str = isodate.duration_isoformat(duration)
+        output = (start_time_str + duration_str).replace("-", "").replace(":", "")
+        return output
+
+    output = (start_time_str).replace("-", "").replace(":", "")
+    return output
+
+
+def main(args):
+    if len(args) > 1:
+        OUTPUTS_DIRECTORY = path.abspath(args[1])
     # Download latest device request list if it doesn't exist
     # This means that the file must be deleted to get a newer version
-    # TODO: Detect the version to keep it up to date
-    if path.exists(DRF_REQUESTS_LIST):
-        read_input(DRF_REQUESTS_LIST)
-    else:
-        get_latest_device_list()
+    latest_version = get_latest_device_list_version()
 
-    # TODO: determine start time
-    start_time = get_start_time()
-    print(start_time)
-    output_filename = ''
-    duration = 600
+    # This always overwrites the file at DRF_REQUESTS_LIST
+    get_latest_device_list()
 
-    # dpmData.main([
-    #     '-s', start_time,
-    #     '-du', duration,
-    #     '-f', DRF_REQUESTS_LIST,
-    #     '-o', output_filename,
-    #     '-d', '1'  # debugging with only one device
-    # ])
+    # get_start_time always returns
+    start_time, duration = get_start_time()
+    end_time = start_time + duration
+    iso_datetime_duration = name_output_file(
+        start_time, duration)
+    request_list_version = latest_version.replace('.', '_')
+    output_filename = f'{iso_datetime_duration}-{request_list_version}.h5'
+    output_path_and_filename = path.join(OUTPUTS_DIRECTORY, output_filename)
+    dpmData.main([
+        '-s', str(start_time),
+        '-e', str(end_time),
+        '-f', DRF_REQUESTS_LIST,
+        '-o', output_path_and_filename,
+        #'-d', '1'  # debugging with only one device
+    ])
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
