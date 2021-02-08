1157fdd6faa34bb66515a1648e5366e719ac932b
sakshi-fermi <snag@fnal.gov>
2021-02-08 14:32:13 -0600

    feat: :sparkles: write data files to a day folder
    closes #14

--- nanny.py
+++ nanny.py
@@ -3,6 +3,7 @@
 
 from glob import glob
 from os import path
+from os import makedirs
 from datetime import datetime
 from datetime import timedelta
 import sys
@@ -48,9 +49,10 @@ def get_latest_device_list(output_path):
 
 def get_start_time(output_path):
     duration = timedelta(hours=1)
-    h5_outputs = path.join(output_path, '*.h5')
+    h5_outputs = path.join(output_path, '**', '*.h5')
     # Glob allows the use of the * wildcard
-    files = glob(h5_outputs)
+    file_paths = glob(h5_outputs, recursive=True)
+    files = list(map(path.basename, file_paths))
     # Sort modifies the list in place
     files.sort()
 
@@ -85,12 +87,22 @@ def name_output_file(start_time, duration=None):
     return output
 
 
+def create_structured_path(outputs_directory, start_time):
+    # YYYYMM
+    month_directory = f'{start_time.year}{start_time.month:02d}'
+    # DD
+    day_directory = f'{start_time.day:02d}'
+
+    return path.join(outputs_directory, month_directory, day_directory)
+
+
 def main(args):
     drf_request_list = 'linac_logger_drf_requests.txt'
     outputs_directory = path.abspath('.')
 
     if len(args) > 1:
         outputs_directory = path.abspath(args[1])
+
     # Download latest device request list if it doesn't exist
     # This means that the file must be deleted to get a newer version
     latest_version = get_latest_device_list_version()
@@ -100,13 +112,16 @@ def main(args):
 
     # get_start_time always returns
     start_time, duration = get_start_time(outputs_directory)
+    structured_outputs_directory = create_structured_path(
+        outputs_directory, start_time)
     end_time = start_time + duration
     iso_datetime_duration = name_output_file(
         start_time, duration)
     request_list_version = latest_version.replace('.', '_')
     output_filename = f'{iso_datetime_duration}-{request_list_version}.h5'
     temp_path_and_filename = path.join('.', output_filename)
-    output_path_and_filename = path.join(outputs_directory, output_filename)
+    output_path_and_filename = path.join(
+        structured_outputs_directory, output_filename)
 
     # Being data request writing to local file
     dpm_data.main([
@@ -114,8 +129,12 @@ def main(args):
         '-e', str(end_time),
         '-f', drf_request_list,
         '-o', temp_path_and_filename,
-        # '-d', '1'  # debugging with only one device
+        #'-d', '1'  # debugging with only one device
     ])
+
+    # Ensure that the folders exist
+    if not path.exists(structured_outputs_directory):
+        makedirs(structured_outputs_directory, exist_ok=True)
 
     # Move local closed file to final destination
     shutil.move(temp_path_and_filename, output_path_and_filename)
