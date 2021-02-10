965e53f862d6ab8a29b65e1fae5ad1de8c0da371
sakshi-fermi <snag@fnal.gov>
2021-02-10 10:10:38 -0600

    feat: :sparkles: Generate files from the calculated start time until now
    Closes #19

--- nanny.py
+++ nanny.py
@@ -12,7 +12,6 @@ import isodate
 import requests
 import dpm_data
 
-
 def write_output(file, output):
     with open(file, 'w+') as file_handle:
         for line in output:
@@ -111,34 +110,38 @@ def main(args):
     get_latest_device_list(drf_request_list)
 
     # get_start_time always returns
+    current_time = datetime.now()
     start_time, duration = get_start_time(outputs_directory)
-    structured_outputs_directory = create_structured_path(
-        outputs_directory, start_time)
     end_time = start_time + duration
-    iso_datetime_duration = name_output_file(
-        start_time, duration)
-    request_list_version = latest_version.replace('.', '_')
-    output_filename = f'{iso_datetime_duration}-{request_list_version}.h5'
-    temp_path_and_filename = path.join('.', output_filename)
-    output_path_and_filename = path.join(
-        structured_outputs_directory, output_filename)
 
-    # Being data request writing to local file
-    dpm_data.main([
-        '-s', str(start_time),
-        '-e', str(end_time),
-        '-f', drf_request_list,
-        '-o', temp_path_and_filename,
-        #'-d', '1'  # debugging with only one device
-    ])
+    while current_time > end_time:
+        structured_outputs_directory = create_structured_path(
+            outputs_directory, start_time)
+        iso_datetime_duration = name_output_file(
+            start_time, duration)
+        request_list_version = latest_version.replace('.', '_')
+        output_filename = f'{iso_datetime_duration}-{request_list_version}.h5'
+        temp_path_and_filename = path.join('.', output_filename)
+        output_path_and_filename = path.join(
+            structured_outputs_directory, output_filename)
 
-    # Ensure that the folders exist
-    if not path.exists(structured_outputs_directory):
-        makedirs(structured_outputs_directory, exist_ok=True)
+        # Being data request writing to local file
+        dpm_data.main([
+            '-s', str(start_time),
+            '-e', str(end_time),
+            '-f', drf_request_list,
+            '-o', temp_path_and_filename,
+            #'-d', '10'  # debugging with only one device
+        ])
 
-    # Move local closed file to final destination
-    shutil.move(temp_path_and_filename, output_path_and_filename)
+        # Ensure that the folders exist
+        if not path.exists(structured_outputs_directory):
+            makedirs(structured_outputs_directory, exist_ok=True)
 
+        # Move local closed file to final destination
+        shutil.move(temp_path_and_filename, output_path_and_filename)
+        start_time = end_time
+        end_time = start_time + duration
 
 if __name__ == '__main__':
     main(sys.argv)
