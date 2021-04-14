19d3b2a5a541e2a452f156c7b45deabecaee89dd
Beau Harrison <beau@fnal.gov>
2021-04-14 08:57:32 -0500

    fix: :bug: Correctly fallback on _ key word args.

--- datalogger_to_ml/h5_dump.py
+++ datalogger_to_ml/h5_dump.py
@@ -6,11 +6,11 @@ from . import helper_methods
 
 
 def dump(**kwargs):
-    with pd.HDFStore(kwargs.get('input-file').resolve(), 'r') as hdf:
+    with pd.HDFStore(kwargs.get('input-file', kwargs.get('input_file')), 'r') as hdf:
         output = []
 
         for key in list(hdf.keys()):
             data_frame = hdf[key]
             output.append(f'{key}:\n{data_frame}')
 
-        helper_methods.write_output(kwargs.get('output-file'), output)
+        helper_methods.write_output(kwargs.get('output-file', kwargs.get('output_file')), output)
